a49613bd9570730ace3687c0b6e616f8c6e7c0d5
Fábio Lameira <fabiolameira97@gmail.com>
2019-08-15 12:12:18 +0100

    Initial Commit

---
+++ PDMII_Exercicio_10/PDMII_Exercicio_10/platforms/android/assets/www/scripts/strings.js
@@ -0,0 +1,37 @@
+﻿var strings = {
+    en: {
+        btnInsert: 'Insert Person',
+        btnListView: 'List Persons',
+        birthDate: 'Birthdate',
+        town: 'Town',
+        email: 'Email',
+        phone: 'Phone'
+    },
+
+    pt: {
+        btnInsert: 'Inserir Pessoa',
+        btnListView: 'Listar Pessoas',
+        birthDate: 'Data de nascimento',
+        town: 'Localidade',
+        email: 'Email',
+        phone: 'Telemóvel'
+    },
+
+    fr: {
+        btnInsert: 'Insérer personne',
+        btnListView: 'Listar Pessoas',
+        birthDate: 'Date de naissance',
+        town: 'Ville',
+        email: 'Email',
+        phone: 'Téléphone'
+    },
+
+    de: {
+        btnInsert: 'Person einfügen',
+        btnListView: 'Personen auflisten',
+        birthDate: 'Geburtstag',
+        town: 'Stadt',
+        email: 'Email',
+        phone: 'Telefon'
+    }
+}
