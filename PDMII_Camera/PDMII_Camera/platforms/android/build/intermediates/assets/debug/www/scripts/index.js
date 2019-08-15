7ec34614153c0ea106af1d2774bf9cd2dafb8702
Fábio Lameira <fabiolameira97@gmail.com>
2019-08-15 12:47:37 +0100

    Initial Commit

---
+++ PDMII_Camera/PDMII_Camera/platforms/android/build/intermediates/assets/debug/www/scripts/index.js
@@ -0,0 +1,58 @@
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkID=397704
+// To debug code on page load in cordova-simulate or on Android devices/emulators: launch your app, set breakpoints, 
+// and then run "window.location.reload()" in the JavaScript Console.
+(function () {
+    "use strict";
+
+    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+
+    function onDeviceReady() {
+
+        document.getElementById("btnChooseFile").addEventListener('touchend', openFilePicker, false);
+
+    };
+
+    function openFilePicker() {
+
+        var srcType = Camera.PictureSourceType.SAVEDPHOTOALBUM;
+        var options = setOptions(srcType);
+
+
+        navigator.camera.getPicture(function cameraSuccess(imageUri) {
+
+            displayImage(imageUri);
+
+        }, function cameraError(error) {
+            console.debug("Unable to obtain picture: " + error, "app");
+
+        }, options);
+
+    }
+
+    function setOptions(srcType) {
+
+        var options = {
+            // Some common settings are 20, 50, and 100
+            quality: 100,
+            destinationType: Camera.DestinationType.FILE_URI,
+            // In this app, dynamically set the picture source, Camera or photo gallery
+            sourceType: srcType,
+            encodingType: Camera.EncodingType.JPEG,
+            mediaType: Camera.MediaType.PICTURE,
+            allowEdit: false,
+            correctOrientation: true  //Corrects Android orientation quirks
+            //targetHeight: 500,
+            //targetWidth: 500
+        };
+        return options;
+    }
+
+    function displayImage(imageUri) {
+
+        var element = document.getElementById('imgFile');
+        element.src = imageUri;
+
+    }
+
+} )();
